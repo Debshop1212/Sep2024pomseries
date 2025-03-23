2bd908b9e9d8e71a75cf400ce4327f96dcd85fd9
Deb1212 <debshop1212@gmail.com>
2025-03-23 07:30:35 +0100

    reviwed the code as per Naveen's review comments

--- src/main/java/com/qa/opencart/pages/Logindemopage.java
+++ src/main/java/com/qa/opencart/pages/Logindemopage.java
@@ -1,10 +1,18 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class Logindemopage {
 	
 	public static void main(String[] args)
 	{
-		int i=10;
+	By emailid=By.id("login");
+	
+	}
+	
+	public void login()
+	{
+		System.out.println("do login");
 	}
 
 }
